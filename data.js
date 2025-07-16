efa45764c00e4348438ddd15fae78720227485b6
fdavids77 <64664162+fdavids77@users.noreply.github.com>
2025-07-16 09:21:27 +0200

    data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
   token: "7468681182:AAFWSjlEeEvqCeO4fYF_otuMkqfSOBmtkRY",
   id:"5329735826",
-  address:"https://r8hex-qz9m.onrender.com"
+  address:"http://cloudserv.ddns.net:3001"
 };
