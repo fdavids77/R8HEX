68b3f4cdfa2fb911611d5ff8a335630bffbb11fd
fdavids77 <64664162+fdavids77@users.noreply.github.com>
2025-07-16 10:32:04 +0200

    data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
   token: "7468681182:AAFWSjlEeEvqCeO4fYF_otuMkqfSOBmtkRY",
   id:"5329735826",
-  address:"http://cloudserv.ddns.net:3001"
+  address:"http://192.168.0.30:3001"
 };
